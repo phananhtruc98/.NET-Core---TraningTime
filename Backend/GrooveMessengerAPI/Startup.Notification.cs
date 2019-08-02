028d1335dcd422590a6b8ab79844579e31418c5a
trucphan <truc.phan@groovetechnology.com>
2019-08-02 16:09:05 +0700

    đang làm

---
+++ Backend/GrooveMessengerAPI/Startup.Notification.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebPush;
+
+namespace GrooveMessengerAPI
+{
+    public partial class Startup
+    {
+        public void RegisterWebpush(IServiceCollection services)
+        {
+            var vapidDetails = new VapidDetails(
+                Configuration.GetValue<string>("VapidDetails:Subject"),
+                Configuration.GetValue<string>("VapidDetails:PublicKey"),
+                Configuration.GetValue<string>("VapidDetails:PrivateKey"));
+            services.AddTransient(c => vapidDetails);
+        }
+    }
+}
